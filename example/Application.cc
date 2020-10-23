9a5dec03dbe95ed895c75c26ec078088a77948c2
ThinkAbstract <alexander.lowa.04@gmail.com>
2020-10-23 12:13:27 +0200

    Updated example

--- example/Application.cc
+++ example/Application.cc
@@ -8,32 +8,33 @@ int main(int argc, const char *argv[])
 {
 	if (argc < 2) 
 	{
-		std::cout << "no file specified!" << std::endl;
+		std::cout << "usage: " << argv[0] << " [infile] [outfile]" << std::endl;
 		return 0;
 	}
 
 	Decompressor example_decompressor = Decompressor();
-	std::ifstream file = std::ifstream(argv[1], std::ios_base::binary);
-	
-	if(!file.is_open())
+	std::ifstream input_file = std::ifstream(argv[1], std::ios::binary | std::ios::app | std::ios::in);
+	std::ofstream output_file;
+
+	if(!input_file.is_open())
 	{
-		std::cout << "could not open the specified file!" << std::endl;
+		std::cout << "could not open the specified input file!" << std::endl;
 		return 0;
 	}
 
-	file.seekg(0, std::ios::end);
-	unsigned int compressed_data_size = file.tellg();
-	file.seekg(0, std::ios::beg);
+	input_file.seekg(0, std::ios::end);
+	unsigned int compressed_data_size = input_file.tellg();
+	input_file.seekg(0, std::ios::beg);
 
 	unsigned int max_decompressed_data_size = 200000000; // As an example
 	unsigned char *compressed_data = new unsigned char[compressed_data_size];
 	unsigned char *decompressed_data = new unsigned char[max_decompressed_data_size];
 
-	file.read(reinterpret_cast<char *>(&compressed_data[0]), compressed_data_size);
-	file.close();
+	input_file.read(reinterpret_cast<char *>(&compressed_data[0]), compressed_data_size);
+	input_file.close();
 
 	unsigned int decompressed_data_size = example_decompressor.Feed(compressed_data,
-		compressed_data_size, decompressed_data, max_decompressed_data_size, true);
+		compressed_data_size, decompressed_data, max_decompressed_data_size, false);
 
 	if (decompressed_data_size == -1)
 	{
@@ -44,7 +45,21 @@ int main(int argc, const char *argv[])
 		return 0;
 	}
 
-	std::cout << "decompressed " << decompressed_data_size << " bytes";
+	std::cout << "decompressed " << decompressed_data_size << " bytes" << std::endl;
+
+	output_file.open(argv[2], std::ios::binary | std::ios::app | std::ios::out);
+
+	if (!output_file.is_open())
+	{
+		std::cout << "could not open the specified output file!" << std::endl;
+		delete[] compressed_data;
+		delete[] decompressed_data;
+		
+		return 0;
+	}
+
+	output_file.write(reinterpret_cast<char *>(&decompressed_data[0]), decompressed_data_size);
+	output_file.close();
 
 	delete[] compressed_data;
 	delete[] decompressed_data;
